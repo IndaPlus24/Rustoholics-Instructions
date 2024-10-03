38c833c6890d325fb33f6df7fc38e9f293ad4c8d
Dmitry Chirin <chirin.dima@gmail.com>
2024-10-04 01:59:25 +0200

    add right user input for numbers at client

--- task-06-sockets/rust-example-simple/client/src/main.rs
+++ task-06-sockets/rust-example-simple/client/src/main.rs
@@ -1,19 +1,36 @@
+use std::io;
 use std::io::prelude::*;
 use std::net::TcpStream;
+
+fn get_u8_input(s: &str) -> u8 {
+    print!("{s}");
+    io::stdout().flush().expect("flush failed!");
+
+    let mut input_line = String::new();
+    io::stdin()
+        .read_line(&mut input_line)
+        .expect("Failed to read line");
+    let result = input_line.trim().parse().expect("Input not an u8");
+
+    println!();
+
+    result
+}
 
 fn main() -> std::io::Result<()> {
     if let Ok(mut socket) = TcpStream::connect("127.0.0.1:1337") {
         println!("Connected to the server!");
 
         // Send a and b to server
-        let (a, b) = (5u8, 6u8);
+        let a = get_u8_input("First number >>> ");
+        let b = get_u8_input("Second number >>> ");
         socket.write(&[a, b])?;
         socket.flush().expect("Couldn't Flush the message to the socket");
         println!("Send to server: {a}, {b}");
 
-        // Recieve the result
+        // Receive the result
         let mut buf = [0; 1];
-        socket.read(&mut buf).expect("Cound't read from the socket");
+        socket.read(&mut buf).expect("Couldn't read from the socket");
         let c = &buf[0];
         println!("Received from server: {c}");
 
