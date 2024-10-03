5c6fb53e72d8e4e7b52ee5fb6db965cc597d523f
Dmitry Chirin <chirin.dima@gmail.com>
2024-10-04 00:36:17 +0200

    add loop server

--- task-06-sockets/rust-example-simple/server/src/main.rs
+++ task-06-sockets/rust-example-simple/server/src/main.rs
@@ -1,14 +1,14 @@
 use std::net::{TcpListener, TcpStream};
 
-fn main() -> std::io::Result<()> {
+fn main() {
     let listener = TcpListener::bind("127.0.0.1:1337").unwrap();
 
-    match listener.accept() {
-        Ok((_socket, addr)) => println!("new client: {addr:?}"),
-        Err(e) => println!("couldn't get client: {e:?}"),
-    };
-
-    Ok(())
+    loop {
+        match listener.accept() {
+            Ok((_socket, addr)) => println!("new client: {addr:?}"),
+            Err(e) => println!("couldn't get client: {e:?}"),
+        };
+    }
 }
 
 
