b398a67999f075fa96f4a76e32680b25130182dc
Dmitry Chirin <chirin.dima@gmail.com>
2024-10-04 01:15:26 +0200

    add sending data through stream from the client, and receiving by the server

--- task-06-sockets/rust-example-simple/client/src/main.rs
+++ task-06-sockets/rust-example-simple/client/src/main.rs
@@ -5,11 +5,12 @@ fn main() -> std::io::Result<()> {
     if let Ok(mut stream) = TcpStream::connect("127.0.0.1:1337") {
         println!("Connected to the server!");
 
-        stream.write(&[1])?;
-        stream.read(&mut [0; 128])?;
+        stream.write(&[1, 2])?;
+        stream.flush()
+        // stream.read(&mut [0; 128])?;
     } else {
         println!("Couldn't connect to server...");
+        // Works for now, but should be an error
+        Ok(())
     }
-
-    Ok(())
 }
--- task-06-sockets/rust-example-simple/server/src/main.rs
+++ task-06-sockets/rust-example-simple/server/src/main.rs
@@ -1,16 +1,21 @@
+use std::io::Read;
 use std::net::{TcpListener, TcpStream};
+use std::path::Component::Prefix;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:1337").unwrap();
 
     loop {
         match listener.accept() {
-            Ok((socket, addr)) => {
+            Ok((mut socket, addr)) => {
                 println!("New client: {addr:?}");
-
+                let mut buf = [0; 2];
+                socket.read(&mut buf).expect("Couldn't read from socket");
+                println!("From buffer: {buf:?}\n");
             },
             Err(e) => println!("Couldn't get client: {e:?}"),
         };
+
     }
 }
 
