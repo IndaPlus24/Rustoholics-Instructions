fcb49dcc8c5be086a27e864661f64c6528af258f
Dmitry Chirin <chirin.dima@gmail.com>
2025-02-07 02:06:37 +0100

    add comments to tests

--- task-17-trees/tree_template/src/lib.rs
+++ task-17-trees/tree_template/src/lib.rs
@@ -7,12 +7,13 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use rstest::*;
 
-/// An example of
+/// An example of a function using BST to sort a vec
 fn tree_sort(mut input: Vec<i32>) -> Vec<i32> {
     input.sort();
     return input;
 }
 
+// Simple tests
 #[rstest]
 #[case(1)]
 #[case(2)]
@@ -23,12 +24,14 @@ fn tree_sort(mut input: Vec<i32>) -> Vec<i32> {
 #[case(7)]
 #[case(8)]
 fn run_tests(#[case] test: i32) {
+    // Get test file names, input and poutput
     let test_name = "tests/test_".to_owned() + &test.to_string();
     let inp = "_input.txt";
     let out = "_output.txt";
     let reader_inp = BufReader::new(File::open(test_name.to_owned() + inp).expect("Can't open file"));
     let reader_out = BufReader::new(File::open(test_name.to_owned() + out).expect("Can't open file"));
 
+    // Read input file
     let mut unsorted: Vec<i32> = vec![];
     for line in reader_inp.lines().skip(1) {
         for word in line.unwrap().split_whitespace() {
@@ -36,6 +39,7 @@ fn run_tests(#[case] test: i32) {
         }
     }
 
+    // Read output file
     let mut sorted: Vec<i32> = vec![];
     for line in reader_out.lines().skip(1) {
         for word in line.unwrap().split_whitespace() {
@@ -44,5 +48,6 @@ fn run_tests(#[case] test: i32) {
         }
     }
 
+    // Compare
     assert_eq!(tree_sort(unsorted.clone()), sorted);
 }
