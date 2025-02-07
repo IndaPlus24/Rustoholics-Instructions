553fa82ff1cefcb0b4d5bd82714fd6769e99aadf
Dmitry Chirin <chirin.dima@gmail.com>
2025-02-07 02:03:55 +0100

    tests work

---
+++ task-17-trees/tree_template/src/lib.rs
@@ -0,0 +1,48 @@
+/***
+ * Template for an implementation of a BST.
+ * Author: Dmitry Chirin <dmitryc@kth.se>
+ */
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use rstest::*;
+
+/// An example of
+fn tree_sort(mut input: Vec<i32>) -> Vec<i32> {
+    input.sort();
+    return input;
+}
+
+#[rstest]
+#[case(1)]
+#[case(2)]
+#[case(3)]
+#[case(4)]
+#[case(5)]
+#[case(6)]
+#[case(7)]
+#[case(8)]
+fn run_tests(#[case] test: i32) {
+    let test_name = "tests/test_".to_owned() + &test.to_string();
+    let inp = "_input.txt";
+    let out = "_output.txt";
+    let reader_inp = BufReader::new(File::open(test_name.to_owned() + inp).expect("Can't open file"));
+    let reader_out = BufReader::new(File::open(test_name.to_owned() + out).expect("Can't open file"));
+
+    let mut unsorted: Vec<i32> = vec![];
+    for line in reader_inp.lines().skip(1) {
+        for word in line.unwrap().split_whitespace() {
+            unsorted.push(word.parse().unwrap());
+        }
+    }
+
+    let mut sorted: Vec<i32> = vec![];
+    for line in reader_out.lines().skip(1) {
+        for word in line.unwrap().split_whitespace() {
+            sorted.push(word.parse().unwrap());
+            println!("word '{}'", word);
+        }
+    }
+
+    assert_eq!(tree_sort(unsorted.clone()), sorted);
+}
